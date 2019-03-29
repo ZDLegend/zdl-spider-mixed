c7cce237725b09c2b7d4bb2384e10ebacc0622f9
张明浩 <463764941@qq.com>
2019-03-29 16:29:49 +0800

    add FileUtil

---
+++ src/main/java/com/zdl/spider/mixed/utils/FileUtil.java
@@ -0,0 +1,42 @@
+package com.zdl.spider.mixed.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * 文件操作工具类
+ *
+ * Created by ZDLegend on 2019/3/29 16:05
+ */
+public class FileUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(FileUtil.class);
+
+    /**
+     * create file by file`s path
+     *
+     * @param path file`s path
+     * @return File object
+     */
+    public static File createFile(String path){
+        File file = new File(path);
+        File fileParent = file.getParentFile();
+        if(!fileParent.exists()){
+            boolean is = fileParent.mkdirs();
+            logger.debug("create dirs {} : {}", fileParent.getAbsolutePath(), is);
+        }
+
+        try {
+            boolean is = file.createNewFile();
+            logger.debug("create file {} : {}", file.getAbsolutePath(), is);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+
+        return file;
+    }
+
+}
