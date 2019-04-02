139d1169fe0755ab0d943d94fc03bfc654cba553
张明浩 <463764941@qq.com>
2019-04-02 19:53:47 +0800

    add JsoupUtil for analyzing html

---
+++ src/main/java/com/zdl/spider/mixed/utils/JsoupUtil.java
@@ -0,0 +1,24 @@
+package com.zdl.spider.mixed.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by ZDLegend on 2019/4/2 19:40
+ */
+public class JsoupUtil {
+
+    public static List<String> getImageAddrByHtml(String html){
+        Document doc = Jsoup.parseBodyFragment(html);
+        Element body = doc.body();
+        Elements getDiv = body.getElementsByTag("img");
+        return getDiv.stream()
+                .map(element -> element.attr("src"))
+                .collect(Collectors.toList());
+    }
+}
