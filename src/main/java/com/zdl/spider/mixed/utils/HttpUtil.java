ac75e15793d11b07f92dce1e2ed1f1684c30e602
张明浩 <463764941@qq.com>
2019-04-25 08:34:52 +0800

    remove the unsed import

--- src/main/java/com/zdl/spider/mixed/utils/HttpUtil.java
+++ src/main/java/com/zdl/spider/mixed/utils/HttpUtil.java
@@ -12,7 +12,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
