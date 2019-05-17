c47aa61a433fb87ad575a303a1e55624b2829e6d
张明浩 <463764941@qq.com>
2019-05-17 18:04:09 +0800

    Update Content.java

--- src/main/java/com/zdl/spider/mixed/zhihu/resources/Content.java
+++ src/main/java/com/zdl/spider/mixed/zhihu/resources/Content.java
@@ -1,7 +1,13 @@
 package com.zdl.spider.mixed.zhihu.resources;
 
+import com.zdl.spider.mixed.utils.FileUtil;
 import com.zdl.spider.mixed.zhihu.entity.AnswerEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -11,6 +17,10 @@ import java.util.concurrent.CompletableFuture;
  */
 public class Content implements Resource  {
 
+    private static Logger logger = LoggerFactory.getLogger(Content.class);
+
+    private String name;
+
     /**
      * 内容所在回答
      */
@@ -19,18 +29,38 @@ public class Content implements Resource  {
     public static Content of( AnswerEntity answerEntity) {
         Content content = new Content();
         content.answerEntity = answerEntity;
+        content.name = answerEntity.getName();
         return content;
     }
 
     public CompletableFuture<Void> directSave(String localPath) {
-        return null;
+        return saveContent(localPath, name);
     }
 
     public CompletableFuture<Void> saveByAuthorThenQuestion(String basePath) {
-        return null;
+        String absolutePath = basePath
+                + File.separator + FileUtil.removeIllegalWord(answerEntity.getAuthor().getName())
+                + File.separator + FileUtil.removeIllegalWord(answerEntity.getQuestion().getTitle());
+        return saveContent(absolutePath, name);
     }
 
     public CompletableFuture<Void> saveByQuestionThenAuthor(String basePath) {
-        return null;
+        String absolutePath = basePath
+                + File.separator + FileUtil.removeIllegalWord(answerEntity.getQuestion().getTitle())
+                + File.separator + FileUtil.removeIllegalWord(answerEntity.getAuthor().getName());
+        return saveContent(absolutePath, name);
+    }
+
+    private CompletableFuture<Void> saveContent(String filePath, String name){
+        String path = filePath + File.separator + FileUtil.removeIllegalWord(name);
+        logger.debug("save to {}", path);
+        FileUtil.createFile(path);
+        return CompletableFuture.runAsync(() -> {
+            try (FileOutputStream fileOutputStream = new FileOutputStream(new File(path))) {
+                fileOutputStream.write(answerEntity.getContent().getBytes());
+            } catch (IOException e) {
+                logger.error("file:{}\nexception:{}", path, e.getMessage(), e);
+            }
+        });
     }
 }
