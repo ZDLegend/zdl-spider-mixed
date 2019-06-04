eac97414a953548871fa02ba8be26a28eaa56d15
张明浩 <463764941@qq.com>
2019-06-04 11:07:59 +0800

    添加知乎问题回答保存数据库

--- src/main/java/com/zdl/spider/mixed/zhihu/web/controller/ZhuHuController.java
+++ src/main/java/com/zdl/spider/mixed/zhihu/web/controller/ZhuHuController.java
@@ -1,7 +1,24 @@
 package com.zdl.spider.mixed.zhihu.web.controller;
 
+import com.zdl.spider.mixed.zhihu.web.service.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by ZDLegend on 2019/5/29 14:53
  */
+@RestController
+@RequestMapping("api/v1/zhihu")
 public class ZhuHuController {
+
+    @Autowired
+    private AnswerService answerService;
+
+    @PostMapping("question/{questionId}")
+    public void saveAllAnswerByQuestion(@PathVariable String questionId) {
+        answerService.saveAllAnswerByQuestion(questionId);
+    }
 }
--- src/main/java/com/zdl/spider/mixed/zhihu/web/service/AnswerService.java
+++ src/main/java/com/zdl/spider/mixed/zhihu/web/service/AnswerService.java
@@ -1,10 +1,14 @@
 package com.zdl.spider.mixed.zhihu.web.service;
 
+import com.zdl.spider.mixed.zhihu.dto.AnswerDto;
+import com.zdl.spider.mixed.zhihu.parser.QuestionParser;
 import com.zdl.spider.mixed.zhihu.web.dao.AnswerDao;
 import com.zdl.spider.mixed.zhihu.web.entity.AnswerEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import static com.zdl.spider.mixed.zhihu.ZhihuConst.MAX_DEEP;
 
 /**
  * Created by ZDLegend on 2019/6/4 10:06
@@ -20,4 +24,18 @@ public class AnswerService extends AbstractService<AnswerEntity> {
         return answerDao;
     }
 
+    /**
+     * 保存某个问题下所有回答进数据库
+     *
+     * @param questionId 问题id
+     */
+    public void saveAllAnswerByQuestion(String questionId) {
+        QuestionParser.getInstance()
+                .pagingParser(questionId, MAX_DEEP,
+                        parser -> parser.contents()
+                                .stream()
+                                .map(AnswerDto::toEntity)
+                                .forEach(this::insert)
+                );
+    }
 }
