3c660a6b39fa2dc267288f9a4efb7bdae6aa9ccb
张明浩 <463764941@qq.com>
2019-06-06 15:05:54 +0800

    Update ContentAnalysis.java

--- src/main/java/com/zdl/spider/mixed/zhihu/analysis/spouse/ContentAnalysis.java
+++ src/main/java/com/zdl/spider/mixed/zhihu/analysis/spouse/ContentAnalysis.java
@@ -24,11 +24,50 @@ public class ContentAnalysis {
 
         //通过关键字，一段一段解析
         list.forEach(s -> {
-
+            analysisGender(s, spouseEntity);
+            analysisAge(s, spouseEntity);
+            analysisHigh(s, spouseEntity);
+            analysisWeigh(s, spouseEntity);
+            analysisEarth(s, spouseEntity);
+            analysisEducation(s, spouseEntity);
         });
 
         spouseEntity.setContent(String.join("\n", list));
 
         return spouseEntity;
+    }
+
+    private static void analysisGender(String sem, SpouseEntity spouseEntity) {
+        if(spouseEntity.getGender() == null || spouseEntity.getGender() == 2) {
+
+            if(sem.contains("本人女") || sem.startsWith("女") || sem.contains("性别女") || sem.contains("：女")) {
+                spouseEntity.setGender(0);
+                return;
+            }
+
+            if(sem.contains("本人男") || sem.startsWith("男") || sem.contains("性别男") || sem.contains("：男")) {
+                spouseEntity.setGender(1);
+            }
+        }
+    }
+
+    private static void analysisAge(String sem, SpouseEntity spouseEntity) {
+
+    }
+
+    private static void analysisHigh(String sem, SpouseEntity spouseEntity) {
+
+    }
+
+    private static void analysisWeigh(String sem, SpouseEntity spouseEntity) {
+
+    }
+
+    private static void analysisEarth(String sem, SpouseEntity spouseEntity) {
+
+    }
+
+    private static void analysisEducation(String sem, SpouseEntity spouseEntity) {
+
     }
 }
