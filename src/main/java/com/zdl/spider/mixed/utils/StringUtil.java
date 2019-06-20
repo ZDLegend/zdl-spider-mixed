55465055513480ead32c424b05570e50dce20abb
张明浩 <463764941@qq.com>
2019-06-20 16:08:22 +0800

    Update StringUtil.java

--- src/main/java/com/zdl/spider/mixed/utils/StringUtil.java
+++ src/main/java/com/zdl/spider/mixed/utils/StringUtil.java
@@ -13,12 +13,12 @@ import java.util.stream.Stream;
 public class StringUtil {
 
     // 判断一个字符串是否都为数字
-    public boolean isDigit(String strNum) {
+    public static boolean isDigit(String strNum) {
         return strNum.matches("[0-9]+");
     }
 
     //提取字符串中所有数字
-    public List<Integer> getDigit(String content) {
+    public static List<Integer> getDigit(String content) {
         String[] cs = Pattern.compile("[^0-9]+").split(content);
         return Stream.of(cs).distinct()
                 .filter(StringUtils::isNotBlank)
