3c32ca0d12a71b993d2b3906207c9b404bd8ad3f
张明浩 <463764941@qq.com>
2019-07-02 11:10:51 +0800

    完善全国省市

--- src/main/java/com/zdl/spider/mixed/zhihu/analysis/spouse/PcParser.java
+++ src/main/java/com/zdl/spider/mixed/zhihu/analysis/spouse/PcParser.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.zdl.spider.mixed.utils.FileUtil;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +19,13 @@ public class PcParser {
 
     static {
         getFile();
+        pcMap.put("香港", new ArrayList<>());
+        pcMap.put("澳门", new ArrayList<>());
+        pcMap.put("台湾", new ArrayList<>());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pcMap);
     }
 
     private static void getFile() {
@@ -29,7 +37,22 @@ public class PcParser {
                     .map(String::valueOf)
                     .map(s1 -> s1.replace("市","").replace("区",""))
                     .collect(Collectors.toList());
-            String ss = key.replace("省", "").replace("市","");
+            String ss = key.replace("省", "")
+                    .replace("市","")
+                    .replace("自治区","");
+
+            if(ss.contains("新疆")) {
+                ss = "新疆";
+            } else if(ss.contains("西藏")) {
+                ss = "西藏";
+            } else if(ss.contains("广西")) {
+                ss = "广西";
+            } else if(ss.contains("内蒙古")) {
+                ss = "内蒙古";
+            } else if(ss.contains("宁夏")) {
+                ss = "宁夏";
+            }
+
             pcMap.put(ss, list);
         });
     }
