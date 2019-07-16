d4c6baf290d357ac63ae9163c01652826caeb141
张明浩 <463764941@qq.com>
2019-07-16 22:03:02 +0800

    添加回答问题的map的存放类

---
+++ src/main/java/com/zdl/spider/mixed/zhihu/analysis/spouse/SpouseConst.java
@@ -0,0 +1,13 @@
+package com.zdl.spider.mixed.zhihu.analysis.spouse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SpouseConst {
+
+    public static Map<String, String> questionMap = new HashMap<>();
+
+    static {
+
+    }
+}
