90e8d98d37308e90b407493eba950fe64220a006
张明浩 <463764941@qq.com>
2019-07-17 21:43:33 +0800

    Update SpouseConst.java

--- src/main/java/com/zdl/spider/mixed/zhihu/analysis/spouse/SpouseConst.java
+++ src/main/java/com/zdl/spider/mixed/zhihu/analysis/spouse/SpouseConst.java
@@ -1,13 +1,31 @@
 package com.zdl.spider.mixed.zhihu.analysis.spouse;
 
+import com.zdl.spider.mixed.zhihu.parser.SearchParser;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class SpouseConst {
 
     public static Map<String, String> questionMap = new HashMap<>();
 
     static {
+        getAllQuestionBySpouse();
+    }
 
+    private static CompletableFuture<Void> getAllQuestionBySpouse() {
+        return SearchParser.getInstance()
+                .execute("择偶标准是怎样的", 0, 100)
+                .thenAccept(parser -> parser.contents().forEach(answerDto -> {
+                    if(!questionMap.containsKey(answerDto.getQuestion().getId())) {
+                        questionMap.put(answerDto.getQuestion().getId(), answerDto.getQuestion().getName());
+                    }
+                }));
+    }
+
+    public static void main(String[] args) {
+        getAllQuestionBySpouse().join();
+        System.out.println(questionMap);
     }
 }
