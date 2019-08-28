c61b1361b7572ff39cde24243c9fe0708f8c4400
张明浩 <463764941@qq.com>
2019-08-28 21:47:34 +0800

    添加解析借口

--- src/main/java/com/zdl/spider/mixed/zhihu/web/controller/ZhuHuController.java
+++ src/main/java/com/zdl/spider/mixed/zhihu/web/controller/ZhuHuController.java
@@ -1,6 +1,7 @@
 package com.zdl.spider.mixed.zhihu.web.controller;
 
 import com.zdl.spider.mixed.zhihu.web.service.AnswerService;
+import com.zdl.spider.mixed.zhihu.web.service.SpouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +18,16 @@ public class ZhuHuController {
     @Autowired
     private AnswerService answerService;
 
+    @Autowired
+    private SpouseService spouseService;
+
     @PostMapping("question/{questionId}")
     public void saveAllAnswerByQuestion(@PathVariable String questionId) {
         answerService.saveAllAnswerByQuestion(questionId);
+    }
+
+    @PostMapping("question/spouse")
+    public void analysisSpouse() {
+        spouseService.startAnalysis();
     }
 }
