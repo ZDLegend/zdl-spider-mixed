67654260ea6095834b8719fe5690329dd22bbd9c
张明浩 <463764941@qq.com>
2019-08-29 21:42:16 +0800

    Update SpouseConst.java

--- src/main/java/com/zdl/spider/mixed/zhihu/analysis/spouse/SpouseConst.java
+++ src/main/java/com/zdl/spider/mixed/zhihu/analysis/spouse/SpouseConst.java
@@ -1,6 +1,8 @@
 package com.zdl.spider.mixed.zhihu.analysis.spouse;
 
+import com.zdl.spider.mixed.zhihu.dto.AnswerDto;
 import com.zdl.spider.mixed.zhihu.parser.SearchParser;
+import com.zdl.spider.mixed.zhihu.parser.ZhihuParser;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -10,50 +12,51 @@ public class SpouseConst {
 
     public static Map<String, String> questionMap = new ConcurrentHashMap<>();
 
-    static {
-        getAllQuestionBySpouse().join();
-
-        //无用问题过滤
-        questionMap.forEach((k, v) -> {
-            if (v.contains("如何看待")) {
-                questionMap.remove(k);
-            }
-
-            if (v.contains("基友们")) {
-                questionMap.remove(k);
-            }
-
-            if (v.contains("为什么知乎上")) {
-                questionMap.remove(k);
-            }
-
-            if (v.contains("如何根据")) {
-                questionMap.remove(k);
-            }
-
-            if (v.contains("这样的择偶标准")) {
-                questionMap.remove(k);
-            }
-        });
-
-        questionMap.put("330598228", "有什么高端靠谱的相亲途径?");
-    }
-
 
     /**
      * 通过搜索方式获取择偶标准相关的各个问题(从100个答案中过滤)
      */
     private static CompletableFuture<Void> getAllQuestionBySpouse() {
         return SearchParser.getInstance()
-                .pagingParser("择偶标准是怎样的", 100, parser -> parser.contents().forEach(answerDto -> {
-                    if (!questionMap.containsKey(answerDto.getQuestion().getId())) {
-                        questionMap.put(answerDto.getQuestion().getId(), answerDto.getQuestion().getName());
-                    }
-                }));
+                .pagingParser("择偶标准是怎样的", 50, SpouseConst::cc)
+                .whenComplete((aVoid, throwable) -> {
+                    //无用问题过滤
+                    questionMap.forEach((k, v) -> {
+                        if (v.contains("如何看待")) {
+                            questionMap.remove(k);
+                        }
+
+                        if (v.contains("基友们")) {
+                            questionMap.remove(k);
+                        }
+
+                        if (v.contains("为什么知乎上")) {
+                            questionMap.remove(k);
+                        }
+
+                        if (v.contains("如何根据")) {
+                            questionMap.remove(k);
+                        }
+
+                        if (v.contains("这样的择偶标准")) {
+                            questionMap.remove(k);
+                        }
+                    });
+
+                    questionMap.put("330598228", "有什么高端靠谱的相亲途径?");
+                });
+    }
+
+    private static void cc(ZhihuParser<AnswerDto> parser) {
+        parser.contents().forEach(answerDto -> {
+            if (!questionMap.containsKey(answerDto.getQuestion().getId())) {
+                questionMap.put(answerDto.getQuestion().getId(), answerDto.getQuestion().getName());
+            }
+        });
     }
 
     public static void main(String[] args) {
         getAllQuestionBySpouse().join();
-        System.out.println(questionMap);
+        System.out.println(SpouseConst.questionMap);
     }
 }
