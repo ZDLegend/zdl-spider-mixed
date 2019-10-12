20ab8f8e3e3d293e11f3f9727ddb86296730b347
张明浩 <463764941@qq.com>
2019-10-12 17:47:24 +0800

    添加手动获取资源配置

---
+++ src/main/java/com/zdl/spider/mixed/utils/PropertiesUtil.java
@@ -0,0 +1,23 @@
+package com.zdl.spider.mixed.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ResourceBundle;
+
+/**
+ * 手动获取资源配置
+ *
+ * Created by ZDLegend on 2019/8/21 17:46
+ */
+public class PropertiesUtil {
+
+    public static final ResourceBundle RESOURCE = ResourceBundle.getBundle("application");
+
+    /**
+     * 是否配置HTTP代理
+     */
+    public static boolean isHttpPoxy() {
+        return StringUtils.isNotBlank(RESOURCE.getString("http.poxy.addr"))
+                && StringUtils.isNotBlank(RESOURCE.getString("http.poxy.port"));
+    }
+}
